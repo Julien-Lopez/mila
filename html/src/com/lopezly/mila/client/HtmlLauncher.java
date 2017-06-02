5c5bb8902ea5e235ad7ef97d698096a24b7df1d9
Julien Lopez <julien.lopez@lri.fr>
2017-06-02 12:32:52 +0200

    Initial commit

---
+++ html/src/com/lopezly/mila/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lopezly.mila.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lopezly.mila.Mila;
+
+public class HtmlLauncher extends GwtApplication {
+
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(480, 320);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new Mila();
+    }
+}
